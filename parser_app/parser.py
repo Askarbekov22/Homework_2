2a799823249482491a029e480005ce2728d65d81
Askarbekov22 <96426154+Askarbekov22@users.noreply.github.com>
2022-01-28 17:59:32 +0600

    Homework№6

--- parser_app/parser.py
+++ parser_app/parser.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup as BS
 from django.views.decorators.csrf import csrf_exempt
 
-
 HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
@@ -10,13 +9,11 @@ HEADERS = {
 URL = 'https://rezka.ag/films/comedy/page/1/'
 
 
-
-
-
 @csrf_exempt
 def get_html(URL, params=''):
     req = requests.get(URL, headers=HEADERS, params=params)
     return req
+
 
 @csrf_exempt
 def get_data(html):
@@ -26,11 +23,14 @@ def get_data(html):
     for i in items:
         film.append(
             {
+
                 'title': i.find('div', class_='b-content__inline_item-link').get_text(),
-                'image': i.find('div', class_='b-content__inline_item-cover').find('a').find('img').get('src')
+                'image': i.find('div', class_='b-content__inline_item-cover').find('a').find('img').get('src'),
+
             }
         )
     return film
+
 
 @csrf_exempt
 def parser():
